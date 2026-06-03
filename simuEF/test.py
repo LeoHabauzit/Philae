1a5f1f38f9eb52d8ea9c3d8f3f023e9e029a255e
LeoHabauzit <leo.habauzit@univ-grenoble-alpes.fr>
2026-06-03 14:12:15 +0200

    finir plot test un element

--- simuEF/test.py
+++ simuEF/test.py
@@ -5,6 +5,96 @@ from matplotlib import pylab
 
 fd.ModelingSpace("3D")
 
+
+def plot_data(stress_array, strain_array, time):
+    e11 = strain_array[0]
+    e22 = strain_array[1]
+    e33 = strain_array[2]
+    e12 = strain_array[3]
+    e13 = strain_array[4]
+    e23 = strain_array[5]
+    s11 = stress_array[0]
+    s22 = stress_array[1]
+    s33 = stress_array[2]
+    s12 = stress_array[3]
+    s13 = stress_array[4]
+    s23 = stress_array[5]
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 8), sharex=True)
+
+    # ---------- Contraintes s ----------
+    axs[0, 0].plot(time, s11, color="tab:blue")
+    axs[0, 0].set_title("σ11")
+    axs[0, 0].set_ylabel("[MPa]")
+    axs[0, 0].grid(True)
+
+    axs[0, 1].plot(time, s22, color="tab:orange")
+    axs[0, 1].set_title("σ22")
+    axs[0, 1].grid(True)
+
+    axs[0, 2].plot(time, s33, color="tab:green")
+    axs[0, 2].set_title("σ33")
+    axs[0, 2].grid(True)
+
+    axs[1, 0].plot(time, s12, color="tab:red")
+    axs[1, 0].set_title("σ12 ")
+    axs[1, 0].set_xlabel("Temps (s)")
+    axs[1, 0].set_ylabel("[MPa]")
+    axs[1, 0].grid(True)
+
+    axs[1, 1].plot(time, s13, color="tab:purple")
+    axs[1, 1].set_title("σ13")
+    axs[1, 1].set_xlabel("Temps (s)")
+    axs[1, 1].grid(True)
+
+    axs[1, 2].plot(time, s23, color="tab:brown")
+    axs[1, 2].set_title("σ23")
+    axs[1, 2].set_xlabel("Temps (s)")
+    axs[1, 2].grid(True)
+
+    fig.suptitle("Contraintes σ", fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
+    # ---------- Déformations e ----------’
+    fig, axs = plt.subplots(2, 3, figsize=(15, 8), sharex=True)
+
+    axs[0, 0].plot(time, e11, color="tab:blue")
+    axs[0, 0].set_title("ε11")
+    axs[0, 0].set_ylabel("[–]")
+    axs[0, 0].grid(True)
+
+    axs[0, 1].plot(time, e22, color="tab:orange")
+    axs[0, 1].set_title("ε22")
+    axs[0, 1].grid(True)
+
+    axs[0, 2].plot(time, e33, color="tab:green")
+    axs[0, 2].set_title("ε33")
+    axs[0, 2].grid(True)
+
+    axs[1, 0].plot(time, e12, color="tab:red")
+    axs[1, 0].set_title("ε12")
+    axs[1, 0].set_xlabel("Temps (s)")
+    axs[1, 0].set_ylabel("[–]")
+    axs[1, 0].grid(True)
+
+    axs[1, 1].plot(time, e13, color="tab:purple")
+    axs[1, 1].set_title("ε13")
+    axs[1, 1].set_xlabel("Temps (s)")
+    axs[1, 1].grid(True)
+
+    axs[1, 2].plot(time, e23, color="tab:brown")
+    axs[1, 2].set_title("ε23")
+    axs[1, 2].set_xlabel("Temps (s)")
+    axs[1, 2].grid(True)
+
+    fig.suptitle("Déformations ε", fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
+    return time, stress_array, strain_array
+
+
 mesh = fd.mesh.box_mesh(
     nx=2,
     ny=2,
@@ -84,24 +174,19 @@ time = np.linspace(0, 1, 101)
 stress_array = np.zeros((6, n_iter + 1))
 print(stress_array.shape)
 strain_array = np.zeros((6, n_iter + 1))
-all_components = {"XX", "YY", "ZZ", "XY", "XZ", "YZ"}
+all_components = ["XX", "YY", "ZZ", "XY", "XZ", "YZ"]
 for k in range(dataset.n_iter):
     dataset.load(k)
-    for i, component in enumerate(sorted(all_components)):
-        print(i, component)
+    for i, component in enumerate(all_components, start=0):
         data_stress = dataset.get_data(
-            field="Stress", component="XX", data_type="GaussPoint"
+            field="Stress", component=component, data_type="GaussPoint"
         )
         data_strain = dataset.get_data(
-            field="Strain", component="XX", data_type="GaussPoint"
+            field="Strain", component=component, data_type="GaussPoint"
         )
-        stress_array[i, k + 1] = data_stress[0]
-        strain_array[i, k + 1] = data_strain[0]
+        stress_array[i, k + 1] = data_stress[i]
+        strain_array[i, k + 1] = data_strain[i]
 
-    # stress_array[k + 1] = data_stress[0]
-    # strain_array[k + 1] = data_strain[0]
-    print(data_stress[0])
 # plt.plot(time, stress_array)
-plt.plot(time, strain_array)
-plt.show()
+plot_data(stress_array, strain_array, time)
 # fd.viewer(res)
